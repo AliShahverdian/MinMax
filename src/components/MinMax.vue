685bf230cf5d6bd8ef87c2f7e713e19c2fed3918
AliShahverdian <ali.shahverdian@gmail.com>
2023-04-25 22:28:42 +0100

    create a simple component

---
+++ src/components/MinMax.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<button :disabled="isReducingDisabled" @click="reduceNum">-</button>
+		<input type="number" v-model.number="num" />
+		<button :disabled="isIncreasingDisabled" @click="increaseNum">+</button>
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				num: 1,
+				minNum: 1,
+				maxNum: 10,
+			};
+		},
+		computed: {
+			isReducingDisabled() {
+				return this.num === this.minNum;
+			},
+			isIncreasingDisabled() {
+				return this.num === this.maxNum;
+			},
+		},
+		methods: {
+			reduceNum() {
+				this.num--;
+			},
+			increaseNum() {
+				this.num++;
+			},
+		},
+	};
+</script>
